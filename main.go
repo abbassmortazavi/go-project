dfbaaa37b98b28e42023fbaf78f02a38a6c65702
abbasmortazavi <abbassmortazavi@gmail.com>
2026-05-14 10:05:19 +0330

    install chi

--- main.go
+++ main.go
@@ -14,14 +14,34 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1>")
 }
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, r.URL.Path)
+
+//func pathHandler(w http.ResponseWriter, r *http.Request) {
+//	switch r.URL.Path {
+//	case "/":
+//		homeHandler(w, r)
+//	case "/contact":
+//		contactHandler(w, r)
+//	default:
+//		http.Error(w, "404 not found.", http.StatusNotFound)
+//	}
+//}
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "404 not found.", http.StatusNotFound)
+	}
 }
 func main() {
-	http.HandleFunc("/", pathHandler)
-	http.HandleFunc("/contact", contactHandler)
+	var router Router
 	fmt.Println("Listening on port 8080")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		return
 	}
